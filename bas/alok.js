d1c894d9a533009a043c1b3c9091b3e794b97493
Alok-kumar786 <alok.ku.j786@gmail.com>
2025-02-01 11:39:35 +0000

    container config done

---
+++ bas/alok.js
@@ -0,0 +1,13 @@
+const accountId = "12345"
+let accountEmail = "Alok.ku.876567.com"
+var accountPassword = "9876543"
+accountCity = "bihar"
+
+
+console.log(accountId)
+
+
+accountEmail = "a&@df"
+accountPassword = "456789"
+accountCity = "patna"
+console.table([accountId,accountEmail,accountPassword,accountCity])
