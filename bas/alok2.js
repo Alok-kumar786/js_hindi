ce3ea98e0c4da551d83952b1cd14d7dd3d6ca96d
Alok-kumar786 <alok.ku.j786@gmail.com>
2025-02-01 16:21:50 +0000

    conersion of datatype

---
+++ bas/alok2.js
@@ -0,0 +1,11 @@
+let score1 =null
+let score2 = undefined
+
+console.log(typeof score1);
+console.log(typeof string2);
+
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+
+console.log(typeof valueInNumber1 );
+console.log(typeof valueInNumber2 );
